41f390273bd482e8c4fdfe2b872cccef102bdf9d
Keval Gadhiya <gadhiyakev@gmail.com>
2025-05-08 17:44:37 +0530

    8th may

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,27 +1,9 @@
 import { Link } from "react-router-dom";
 import './App.css';
-import Button from '@mui/material/Button';
-import { MdMenuOpen } from "react-icons/md";
 
 const HeaderCompoenent = () => {
     return (
         <header>
-            <div className="header-container">
-                <div className="left-logos">
-                <Link to="/Dashboard" className="d-flex align-items-center">
-                    <img src="assets/logo/logo.svg" className="logo" alt="Logo" />
-                </Link>
-
-                <Link to="/Dashboard" className="d-flex align-items-center">
-                    <img src="assets/logo/logo.svg" className="logo" alt="Logo" />
-                </Link>
-
-                </div>
-                <Link to="/Dashboard" className="d-flex align-items-center">
-                    <img src="assets/logo/logo.svg" className="logo" alt="Logo" />
-                </Link>
-</div>
-
 
         </header>
     );
