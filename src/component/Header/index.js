934806aad59402e16dd2aec368e4178272732f57
Keval Gadhiya <gadhiyakev@gmail.com>
2025-05-09 18:01:07 +0530

    9th may

--- src/component/Header/index.js
+++ src/component/Header/index.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { Link } from "react-router-dom";
 import './App.css';
 import logo from "../../assets/logo/logo.svg"
@@ -6,16 +7,38 @@ import SearchBox from "../Searchbox";
 
 // Icons
 import {
-  HugeiconsIcon,
-  MenuCollapseIcon,
-  Moon02Icon,
-  Notification03Icon,
-  ArrowDown01Icon,
-} from '../Icon/icon.js'; 
+    HugeiconsIcon,
+    MenuCollapseIcon,
+    Moon02Icon,
+    Notification03Icon,
+    ArrowDown01Icon,
+} from '../Icon/icon.js';
 
+// //Menu
+// import Menu from '@mui/material/Menu';
+// import MenuItem from '@mui/material/MenuItem';
+// import ListItemIcon from '@mui/material/ListItemIcon';
+// import Divider from '@mui/material/Divider';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+// import Tooltip from '@mui/material/Tooltip';
+// import PersonAdd from '@mui/icons-material/PersonAdd';
+// import Settings from '@mui/icons-material/Settings';
+// import Logout from '@mui/icons-material/Logout';
+// import { useState } from "react";
 
 
 const HeaderCompoenent = () => {
+
+    //     const [anchorEl, setAnchorEl] = useState(null);
+    //   const open = Boolean(anchorEl);
+    //    const handleClick = (event) => {
+    //     setAnchorEl(event.cu);
+    //   };
+    //    const handleClose = () => {
+    //     setAnchorEl(null);
+    //   };
+
     return (
         <>
             <header>
@@ -33,25 +56,89 @@ const HeaderCompoenent = () => {
                         <div className="col-sm-3 d-flex align-items-center part2">
                             <Button className="rounded-circle mr-3">
                                 <HugeiconsIcon icon={MenuCollapseIcon} />
-                                </Button>
+                            </Button>
                             <SearchBox />
                         </div>
-                        
+
 
                         {/* End Part */}
                         <div className="col-sm-7 d-flex align-items-center justify-content-end part3">
-                               <Button className="rounded-circle mr-3">   
-                            <HugeiconsIcon icon={Moon02Icon} />
+                            <Button className="rounded-circle mr-3">
+                                <HugeiconsIcon icon={Moon02Icon} />
                             </Button>
 
-                            <Button className="rounded-circle mr-3">   
-                            <HugeiconsIcon icon={Notification03Icon} />
+                            <Button className="rounded-circle mr-3">
+                                <HugeiconsIcon icon={Notification03Icon} />
                             </Button>
-                         
+
 
                             {/* Account Menu */}
                             <Button className="myAccount d-flex align-items-center">
-                                
+                                {/* <Menu
+                                    anchorEl={anchorEl}
+                                    id="account-menu"
+                                    open={open}
+                                    onClose={handleClose}
+                                    onClick={handleClose}
+                                    slotProps={{
+                                        paper: {
+                                            elevation: 0,
+                                            sx: {
+                                                overflow: 'visible',
+                                                filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                mt: 1.5,
+                                                '& .MuiAvatar-root': {
+                                                    width: 32,
+                                                    height: 32,
+                                                    ml: -0.5,
+                                                    mr: 1,
+                                                },
+                                                '&::before': {
+                                                    content: '""',
+                                                    display: 'block',
+                                                    position: 'absolute',
+                                                    top: 0,
+                                                    right: 14,
+                                                    width: 10,
+                                                    height: 10,
+                                                    bgcolor: 'background.paper',
+                                                    transform: 'translateY(-50%) rotate(45deg)',
+                                                    zIndex: 0,
+                                                },
+                                            },
+                                        },
+                                    }}
+                                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                >
+                                    <MenuItem onClick={handleClose}>
+                                        <Avatar /> Profile
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <Avatar /> My account
+                                    </MenuItem>
+                                    <Divider />
+                                    <MenuItem onClick={handleClose}>
+                                        <ListItemIcon>
+                                            <PersonAdd fontSize="small" />
+                                        </ListItemIcon>
+                                        Add another account
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <ListItemIcon>
+                                            <Settings fontSize="small" />
+                                        </ListItemIcon>
+                                        Settings
+                                    </MenuItem>
+                                    <MenuItem onClick={handleClose}>
+                                        <ListItemIcon>
+                                            <Logout fontSize="small" />
+                                        </ListItemIcon>
+                                        Logout
+                                    </MenuItem>
+                                </Menu>
+ */}
+
                                 <div className="userImg">
                                     <span className="rounded-circle">
                                         <img
@@ -64,10 +151,10 @@ const HeaderCompoenent = () => {
                                         John Smith
                                     </p>
                                 </div>
-                                <div>
+                                <div className="myAccIcon">
                                     <HugeiconsIcon icon={ArrowDown01Icon} />
                                 </div>
-                           
+
                             </Button>
                         </div>
                     </div>
