0660fb2b59feb228598ed0fc2aca1b7b3611253a
Keval Gadhiya <gadhiyakev@gmail.com>
2025-05-12 15:06:22 +0530

    12th may

--- src/component/UserMenu/index.js
+++ src/component/UserMenu/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Avatar from '@mui/material/Avatar';
-import { Box, p } from '@mui/material'; // Import Material UI components
-import "../UserMenu/menu.css"; // Import custom CSS for styling
+import "../UserMenu/menu.css"
+import { Box, colors, Typography } from '@mui/material'; // Import Box and Typography
 
-// Import SVG icons used in the menu items
+//SVGs
+
 import {
     AvatarAdd,
     Logout,
@@ -13,59 +14,54 @@ import {
     SwitcAccount,
 } from '../../assets/menu/svg.js';
 
-// UserMenu component definition
+
 const UserMenu = ({ anchorEl, open, onClose }) => {
     return (
         <Menu
-            className="menu-class" 
-            anchorEl={anchorEl} 
-            id="account-menu" 
+            className="menu-class"
+            anchorEl={anchorEl}
+            id="account-menu"
             open={open}
             onClose={onClose}
-            onClick={onClose} 
-            PaperProps={{ // Props applied to the Paper component that wraps the menu items
-                className: `custom-user-menu${open ? ' open' : ''}`, 
+            onClick={onClose}
+            PaperProps={{
+                className: `custom-user-menu${open ? ' open' : ''}`,
                 style: {
-                    boxShadow: 'none', // Remove default shadow
-                    padding: '16px 0', // Vertical padding inside the menu
+                    boxShadow: 'none',
+                    padding: '16px 0',
                 },
             }}
-            transformOrigin={{ horizontal: 'right', vertical: 'top' }} 
-            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }} 
+            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
         >
-            {/* User Information Section */}
             <Box className="user-info-border">
                 <Avatar>JS</Avatar>
-                <Box className="user-text-container">
-                    <p variant="body1" className="p-tag">John Smith</p> {/* User's Name */}
-                    <p variant="body2" color="textSecondary" className="p-tag">Email ID</p> {/* User's Email */}
+                <Box sx={{ paddingLeft: '8px'}}>
+                    <p
+                   style={{ fontSize: '16px', fontWeight: '600' }} 
+                        className="p-tag"
+                    >John Smith</p>
+                    <p 
+                    style={{ fontSize: '14px', fontWeight: '400', color:"#6D757C"}} 
+                    className="p-tag">Email ID</p>
                 </Box>
             </Box>
-
-            {/* Menu Items */}
             <MenuItem
                 className="menu-item-custom"
-                onClick={onClose}
-            >
-                <img src={Setting} alt="User Settings" /> <p variant="body1" className="p-tag">User Settings</p>
+                onClick={onClose}>
+                <img src={Setting} /> <p className="p-tag">User Settings</p>
             </MenuItem>
-            <MenuItem
-                className="menu-item-custom"
-                onClick={onClose}
-            >
-                <img src={AvatarAdd} alt="New Login" /> <p variant="body1" className="p-tag">New Login</p>
+            <MenuItem onClick={onClose}
+                className="menu-item-custom">
+                <img src={AvatarAdd} /> <p className="p-tag">New Login</p>
             </MenuItem>
-            <MenuItem
-                className="menu-item-custom"
-                onClick={onClose}
-            >
-                <img src={Logout} alt="Log Out" /> <p variant="body1" className="p-tag">Log Out</p>
+            <MenuItem onClick={onClose}
+                className="menu-item-custom">
+                <img src={Logout} /> <p className="p-tag">Log Out</p>
             </MenuItem>
-            <MenuItem
-                className="menu-item-custom"
-                onClick={onClose}
-            >
-                <img src={SwitcAccount} alt="Switch Account" /> <p variant="body1" className="p-tag">Switch Account</p>
+            <MenuItem onClick={onClose}
+                className="menu-item-custom">
+                <img src={SwitcAccount} /> <p className="p-tag">Switch Account</p>
             </MenuItem>
         </Menu>
     );
