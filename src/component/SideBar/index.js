d4dab383aeeaf04cfb7d19ce01c247eb4c4aaffb
Keval Gadhiya <gadhiyakev@gmail.com>
2025-05-14 17:04:29 +0530

    14th may

--- src/component/SideBar/index.js
+++ src/component/SideBar/index.js
@@ -1,9 +1,21 @@
-const SideBar = () =>{
-    return(
+
+import Button from '@mui/material/Button';
+import "./side-bar.css"
+
+const SideBar = () => {
+    return (
         <>
-        <div className="sidebar">
-            
-        </div>
+            <div className="sidebar">
+                <ul>
+                    <li>
+                        <Button className='w-100'>
+                            <span className='icon'></span>
+                            Text
+                            <span className='arrow'></span>
+                        </Button>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
