932a1694476d324eb0d5f1a4694b6c352be299c1
Keval Gadhiya <gadhiyakev@gmail.com>
2025-05-14 13:46:56 +0530

    14th may

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import { BrowserRouter,  Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import DashboardPage from './page/Dashboard';
 import LoginPage from './page/Login';
 import HeaderCompoenent from './component/Header';
+import SideBar from './component/SideBar';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-    <HeaderCompoenent></HeaderCompoenent>
-    <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-      </Routes>
-    </BrowserRouter>
-
+      <BrowserRouter>
+        <HeaderCompoenent />
+        <div className='main d-flex'>
+          <div className='sidebarWrapper'>
+            <SideBar />
+          </div>
+          <div className='content'>
+             <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Routes>
+          </div>
+        </div>
+       
+      </BrowserRouter>
     </>
 
 
---
+++ src/component/SideBar/index.js
@@ -0,0 +1,9 @@
+const SideBar = () =>{
+    return(
+        <>
+        <h6>Side Bar Design</h6>
+        </>
+    )
+}
+
+export default SideBar;
